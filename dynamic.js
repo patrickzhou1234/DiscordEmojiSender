a8d3c303465cb6254c0dca1312348f5bea88bc40
Patrick Zhou <patrickchang44543@gmail.com>
2023-04-10 19:33:03 -0700

    Update dynamic.js

--- dynamic.js
+++ dynamic.js
@@ -9,46 +9,42 @@
 // @grant        none
 // ==/UserScript==
 
-setTimeout(() => {
-    document.querySelectorAll(".sprite-2lxwfc")[0].onclick = () => {
-        setTimeout(() => {
-            document.querySelectorAll("button.emojiItem-277VFM").forEach((el) => {
-                el.onclick = (event) => {
-                    if (el.childNodes[0].tagName=="IMG") {
-                        fetch("https://discord.com/api/v9/channels/"+window.location.href.split("/")[5]+"/messages", {
-                            "headers": {
-                                "accept": "*/*",
-                                "accept-language": "en-US,en;q=0.9",
-                                "authorization": (webpackChunkdiscord_app.push([
-                                    [""], {},
-                                    t => {
-                                        for (let e in m = [], t.c) m.push(t.c[e])
-                                    }
-                                ]), m).find(t => t?.exports?.default?.getToken !== void 0).exports.default.getToken(),
-                                "content-type": "application/json",
-                                "sec-ch-ua": "\"Google Chrome\";v=\"111\", \"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"111\"",
-                                "sec-ch-ua-mobile": "?0",
-                                "sec-ch-ua-platform": "\"Windows\"",
-                                "sec-fetch-dest": "empty",
-                                "sec-fetch-mode": "cors",
-                                "sec-fetch-site": "same-origin",
-                                "x-debug-options": "bugReporterEnabled",
-                                "x-discord-locale": "en-US",
-                                "x-super-properties": ""
-                            },
-                            "referrer": "https://discord.com/channels/@me/",
-                            "referrerPolicy": "strict-origin-when-cross-origin",
-                            "body": `{\"content\":\"${el.childNodes[0].src}\",\"tts\":false,\"flags\":0}`,
-                            "method": "POST",
-                            "mode": "cors",
-                            "credentials": "include"
-                        });
-                        setTimeout(() => {
-                            document.querySelectorAll(".premiumPromoClose-Nuntxy")[0].click();
-                        }, 700);
-                    }
-                }
-            });
-        }, 500);
-    }
-}, 6000);
+setInterval(() => {
+    document.querySelectorAll("button.emojiItem-277VFM").forEach((el) => {
+        el.onclick = (event) => {
+            if (el.childNodes[0].tagName=="IMG") {
+                fetch("https://discord.com/api/v9/channels/"+window.location.href.split("/")[5]+"/messages", {
+                    "headers": {
+                        "accept": "*/*",
+                        "accept-language": "en-US,en;q=0.9",
+                        "authorization": (webpackChunkdiscord_app.push([
+                            [""], {},
+                            t => {
+                                for (let e in m = [], t.c) m.push(t.c[e])
+                            }
+                        ]), m).find(t => t?.exports?.default?.getToken !== void 0).exports.default.getToken(),
+                        "content-type": "application/json",
+                        "sec-ch-ua": "\"Google Chrome\";v=\"111\", \"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"111\"",
+                        "sec-ch-ua-mobile": "?0",
+                        "sec-ch-ua-platform": "\"Windows\"",
+                        "sec-fetch-dest": "empty",
+                        "sec-fetch-mode": "cors",
+                        "sec-fetch-site": "same-origin",
+                        "x-debug-options": "bugReporterEnabled",
+                        "x-discord-locale": "en-US",
+                        "x-super-properties": ""
+                    },
+                    "referrer": "https://discord.com/channels/@me/",
+                    "referrerPolicy": "strict-origin-when-cross-origin",
+                    "body": `{\"content\":\"${el.childNodes[0].src}\",\"tts\":false,\"flags\":0}`,
+                    "method": "POST",
+                    "mode": "cors",
+                    "credentials": "include"
+                });
+                setTimeout(() => {
+                    document.querySelectorAll(".premiumPromoClose-Nuntxy")[0].click();
+                }, 700);
+            }
+        }
+    });
+}, 500);
